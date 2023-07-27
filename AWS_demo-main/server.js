0a760e692ae01c944356229afad4bf76fd7811a7
Thor7377 <ruturajnhodage@gmail.com>
2023-07-27 19:40:12 +0530

    sdm pract

---
+++ AWS_demo-main/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var fs = require('fs');
+var path=require('path');
+var app = express();
+var _dir = fs.realpathSync('.');
+_dir = _dir+'/public/'
+//server configuration
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile((_dir + '/index.html'));
+ });
+ 
+ app.get('/array', function (req, res) {
+    res.sendFile((_dir + '/Arrays.html'));
+ });
+ 
+ app.get('/try', function (req, res) {
+    res.sendFile((_dir + '/try.html'));
+ });
+
+
+app.get('/hello', function (req, res) {
+    var person={firstName:'Ravi',lastName:'Tambade',age:47};
+    res.send(person);
+});
+
+var server = app.listen(8888, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://localhost:8888", host, port)
+});
+
+  
